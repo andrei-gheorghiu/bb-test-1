2ef7a1e8b86217ac66b4175a413682fd01d1c5ae
Andrei Gheorghiu <andrei.gheorghiu@kmap.io>
2018-10-20 01:32:54 +0100

    ie js polyfills

---
+++ public/assets/js/polyfills-ie.js
@@ -0,0 +1,36 @@
+var windowOnload=window.onload||function(){};window.onload=function(){windowOnload()};
+if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+            i,
+            el = this;
+        do {
+            i = matches.length;
+            while (--i >= 0 && matches.item(i) !== el) {
+            }
+            ;
+        } while ((i < 0) && (el = el.parentElement));
+        return el;
+    };
+}
+if (typeof Object.assign !== 'function') {
+    Object.assign = function (target) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
+}
